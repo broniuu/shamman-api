c69d0b66fd3bd97b27a6c46e1f27babba2cbc929
broniuu <86781217+broniuu@users.noreply.github.com>
2022-10-21 15:56:48 +0200

    initial commit

---
+++ src/main/java/com/example/toikprojekt2022/ToikProject2022Application.java
@@ -0,0 +1,13 @@
+package com.example.toikprojekt2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToikProject2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToikProject2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toikprojekt2022/ToikProject2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toikprojekt2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToikProject2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
