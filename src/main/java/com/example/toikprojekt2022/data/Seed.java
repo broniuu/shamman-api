8a10a810a1ddbde8e1f5ac88e4f005e93f17f926
broniuu <86781217+broniuu@users.noreply.github.com>
2022-10-29 16:10:31 +0200

    Update Seed.java

--- src/main/java/com/example/toikprojekt2022/data/Seed.java
+++ src/main/java/com/example/toikprojekt2022/data/Seed.java
@@ -29,7 +29,9 @@ public class Seed {
     private DishRepository dishRepository;
 
     public void seedData() {
-        List<Restaurant> restaurants = List.of(
+        List<Restaurant> restaurants;
+        if (restaurantRepository.count() == 0){
+        restaurants = List.of(
                 new Restaurant("Restauracja u Jana" ,""),
                 new Restaurant("American Dream" ,""),
                 new Restaurant("Bistro Przepis" ,""),
@@ -48,25 +50,29 @@ public class Seed {
 //        https://restaumatic-production.imgix.net/uploads/restaurants/17770/logo/1507737005.png?auto=compress&crop=focalpoint&fit=clip&h=500&w=500
 //        http://www.rozana-batorego19.pl/wp-content/uploads/2020/02/81894919_110245320506614_5467250534652051456_o-300x254.jpg
 //        https://polnapol-tarnow.pl/wp/wp-content/uploads/2020/08/pnp_logo_2020-sierpien.png
-        restaurantRepository.saveAll(restaurants);
-        Restaurant firstRestaurant = restaurants.get(1);
-        Iterable<Dish> dishes = List.of(
-                new Dish(
-                        "STEK Z POLĘDWICY WOŁOWEJ",
-                        "warzywami pieczonymi w miodzie, z sosem pieprzowym",
-                        75.0,
-                        "https://static.gotujmy.pl/ZDJECIE_PRZEPISU_ETAP/steak-z-poledwicy-wolowej-526206.jpg",
-                        firstRestaurant
-                ),
-                new Dish(
-                        "KOTLET DE VOLAILLE",
-                        "Kotlet de Volaille z frytkami i surówką",
-                        28.00,
-                        "https://bi.im-g.pl/im/99/c5/18/z25975449AMP,Kotlet-de-volaille-z-maslem-i-czosnkiem---klasyk-n.jpg",
-                        firstRestaurant
-                )
-        );
-        dishRepository.saveAll(dishes);
-
+            restaurantRepository.saveAll(restaurants);
+        } else {
+            restaurants = (List<Restaurant>) restaurantRepository.findAll();
+        }
+        if (dishRepository.count() == 0) {
+            Restaurant firstRestaurant = restaurants.get(1);
+            List<Dish> dishes = List.of(
+                    new Dish(
+                            "STEK Z POLĘDWICY WOŁOWEJ",
+                            "warzywami pieczonymi w miodzie, z sosem pieprzowym",
+                            75.0,
+                            "https://static.gotujmy.pl/ZDJECIE_PRZEPISU_ETAP/steak-z-poledwicy-wolowej-526206.jpg",
+                            firstRestaurant
+                    ),
+                    new Dish(
+                            "KOTLET DE VOLAILLE",
+                            "Kotlet de Volaille z frytkami i surówką",
+                            28.00,
+                            "https://bi.im-g.pl/im/99/c5/18/z25975449AMP,Kotlet-de-volaille-z-maslem-i-czosnkiem---klasyk-n.jpg",
+                            firstRestaurant
+                    )
+            );
+            dishRepository.saveAll(dishes);
+        }
     }
 }
