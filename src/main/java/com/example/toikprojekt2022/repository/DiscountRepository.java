3967ba5c5e04172e3b1e0d92fb953291fdef154c
broniuu <86781217+broniuu@users.noreply.github.com>
2022-11-21 21:02:40 +0100

    add discount value

--- src/main/java/com/example/toikprojekt2022/repository/DiscountRepository.java
+++ src/main/java/com/example/toikprojekt2022/repository/DiscountRepository.java
@@ -1,2 +1,9 @@
-package com.example.toikprojekt2022.repository;public interface DiscountRepository {
+package com.example.toikprojekt2022.repository;
+
+import com.example.toikprojekt2022.model.Discount;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface DiscountRepository extends CrudRepository<Discount, UUID> {
 }
